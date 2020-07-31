c6b5b3409919faa1ea8f668190cfa7c481edbb7d
Mohammad Kadkhodaei <mohammadkad@gmail.com>
2020-07-31 13:25:42 +0430

    Add files via upload

---
+++ Convert.py
@@ -0,0 +1,18 @@
+# Convert Video to Text, By : Mohammad Kadkhodaei, 1399-05-10
+
+# 1- use ffmpeg to convert mp4 to mp3                    > ffmpeg -i 1.mp4 1.mp3
+# 2- then convert mp3 to wav                             > ffmpeg -i 1.mp3 1.wav
+# 3- install this library                                > pip install SpeechRecognition
+
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+# open the file
+with sr.AudioFile('1.wav') as source:
+    # load audio to memory
+    audio = r.record(source)
+    # convert from speech to text
+    text = r.recognize_google(audio)
+    print(text)
+
