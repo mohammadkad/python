db8f192a27ae92b230668267c1c8e67d6f031af9
Mohammad Kadkhodaei <mohammadkad@gmail.com>
2025-09-17 09:21:22 +0330

    Update pytorch.py

--- pytorch/pytorch.py
+++ pytorch/pytorch.py
@@ -32,3 +32,38 @@ device = torch.device("cuda")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # ---
 
+# A typical training loop
+# ---
+import torch.nn.functional as F
+
+torch.manual_seed(123)
+model = NeuralNetwork(num_inputs=2, num_outputs=2)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
+
+num_epochs = 3
+
+for epoch in range(num_epochs):
+
+    model.train()
+    for batch_idx, (features, labels) in enumerate(train_loader):
+
+        logits = model(features)
+
+        loss = F.cross_entropy(logits, labels) # Loss function
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        ### LOGGING
+        print(f"Epoch: {epoch+1:03d}/{num_epochs:03d} "f" | Batch {batch_idx:03d}/{len(train_loader):03d} "f" | Train/Val Loss: {loss:.2f}")
+
+    model.eval()
+    # Optional model evaluation
+# ---
+
+
+# Saving and loading models, 1404-06-26
+torch.save(model.state_dict(), "model.pth")
+model.load_state_dict(torch.load("model.pth", weights_only=True))
+
